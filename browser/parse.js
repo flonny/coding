1cc053692646df30c446c9f95594c4961b767934
范跃东 <632296950@qq.com>
2020-05-20 19:24:47 +0800

    tag parser

--- browser/parse.js
+++ browser/parse.js
@@ -1,70 +1,77 @@
-const EOF = Symbol("EOF")
+const EOF = Symbol("EOF");
+let currentToken = {}
 function data(c) {
-    if (c === '<') {
-        return tagOpen;
-    } else if (c === EOF) {
-        return;
-    }
-    else {
-        return data
-    }
+  if (c === "<") {
+    return tagOpen;
+  } else if (c === EOF) {
+    return;
+  } else {
+    return data;
+  }
 }
-
 function tagOpen(c) {
-    if(c ==="/"){
-        return endTagOpen
-    }else if(c.match(/^[a-zA-Z]$/)) {
-        return tagName(c)
-    }
-}
-function tagName(c) {
-    if(c.match(/^[\t\n\f ]$/)) {
-        return beforeAttributeName;
-    }else if (c ==='/') {
-        return selfClosingStartTag;
-    }else if(c.match(/^[a-zA-Z]$/)) {
-        return tagName;
-    }else if (c==='>') {
-        return data;
-    } else {
-        return tagName;
-    }
+  if (c === "/") {
+    return endTagOpen;
+  } else if (c.match(/^[a-zA-Z]$/)) {
+    return tagName(c);
+  }
 }
 function endTagOpen(c) {
-    if(c.match(/^[a-zA-Z]$/)) {
-        return
-    }
+    if (c.match(/^[a-zA-Z]$/)) {
+        currentToken = {
+            type: 'endTag',
+            tagName:""
+        }
+      return tagName(c);
+    }else if(c === '>') {
+
+    }else if(c===EOF){
+
+    }else{}
+  }
+function tagName(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeName;
+  } else if (c === "/") {
+    return selfClosingStartTag;
+  } else if (c.match(/^[a-zA-Z]$/)) {
+    return tagName;
+  } else if (c === ">") {
+    return data;
+  } else {
+    return tagName;
+  }
 }
-function beforeAttributeName(c) { }
-function selfClosingStartTag() { }
 
+function beforeAttributeName(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeName;
+  } else if(c ==='>') {
+      return data
+  }else if(c==='=') {
+      return beforeAttributeValue
+  } else {
+    return beforeAttributeName;
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function beforeAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeValue;
+  }
+}
+function selfClosingStartTag(c) {
+  if (c === ">") {
+    currentToken.isSelfClosing = true;
+    return true;
+  } else if (c === EOF) {
+  }
+}
 
 module.exports.parseHTML = function (html) {
-    let state = data;
-    for (let c of html) {
-        state = state(c)
-    }
-    state = state(EOF)
-}
+  let state = data;
+  for (let c of html) {
+    state = state(c);
+  }
+  state = state(EOF);
+};
