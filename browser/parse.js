8390955252a5f16d19d1a16deedf21cfbf7395b0
范跃东 <632296950@qq.com>
2020-05-20 23:26:18 +0800

    html

--- browser/parse.js
+++ browser/parse.js
@@ -1,42 +1,79 @@
+/**
+ * <html lang=en>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>promiseA+</title>
+      <style>
+        body p{
+          color:#fff
+        }
+      </style>
+    </head>
+    <body>
+      qweqweqweqw
+    </body>
+    </html>
+ */
+
 const EOF = Symbol("EOF");
-let currentToken = {}
+let currentToken = null;
+let currentAttribute = null;
+function emit(token) {
+  console.log(token);
+}
 function data(c) {
   if (c === "<") {
     return tagOpen;
   } else if (c === EOF) {
+    emit({
+      type: "EOF",
+    });
     return;
   } else {
+    emit({
+      type: "text",
+      content: c,
+    });
     return data;
   }
 }
+
 function tagOpen(c) {
   if (c === "/") {
     return endTagOpen;
   } else if (c.match(/^[a-zA-Z]$/)) {
+    currentToken = {
+      type: "startTag",
+      tagName: "",
+    };
     return tagName(c);
   }
 }
+
 function endTagOpen(c) {
-    if (c.match(/^[a-zA-Z]$/)) {
-        currentToken = {
-            type: 'endTag',
-            tagName:""
-        }
-      return tagName(c);
-    }else if(c === '>') {
-
-    }else if(c===EOF){
-
-    }else{}
+  if (c.match(/^[a-zA-Z]$/)) {
+    currentToken = {
+      type: "endTag",
+      tagName: "",
+    };
+    return tagName(c);
+  } else if (c === ">") {
+  } else if (c === EOF) {
+  } else {
   }
+}
+
 function tagName(c) {
   if (c.match(/^[\t\n\f ]$/)) {
     return beforeAttributeName;
   } else if (c === "/") {
     return selfClosingStartTag;
   } else if (c.match(/^[a-zA-Z]$/)) {
+    currentToken.tagName += c;
     return tagName;
   } else if (c === ">") {
+    emit(currentToken);
     return data;
   } else {
     return tagName;
@@ -46,24 +83,83 @@ function tagName(c) {
 function beforeAttributeName(c) {
   if (c.match(/^[\t\n\f ]$/)) {
     return beforeAttributeName;
-  } else if(c ==='>') {
-      return data
-  }else if(c==='=') {
-      return beforeAttributeValue
+  } else if (c === ">" || c === "/" || c === EOF) {
+    return afterAttributeName;
+  } else if (c === "=") {
   } else {
-    return beforeAttributeName;
+    currentAttribute = {
+      name: "",
+      value: "",
+    };
+    return attributeName(c);
+  }
+}
+function afterAttributeName(c) {}
+function attributeName(c) {
+  if (c.match(/^[\t\n\f]$/) || c === "/" || c === ">" || c === EOF) {
+    return afterAttributeName(c);
+  } else if (c === "=") {
+    emit(currentAttribute);
+    return beforeAttributeValue;
+  } else if (c === "/u0000") {
+  } else if (c === '"' || c === "." || c === "<") {
+  } else {
+    currentAttribute.name += c;
+    return attributeName;
   }
 }
 
 function beforeAttributeValue(c) {
-  if (c.match(/^[\t\n\f ]$/)) {
+  if (c.match(/^[\t\n\f ]$/) || c === ">" || c === "/" || c === EOF) {
     return beforeAttributeValue;
+  } else if (c === '"') {
+    return doubleQuotedAttributeValue;
+  } else if (c === "'") {
+    return singleQuotedAttributeValue;
+  } else if (c === ">") {
+    return data;
+  } else {
+    return UnquotedAttributeValue(c);
   }
 }
+
+function doubleQuotedAttributeValue(c) {
+  if (c === '"') {
+    return afterQuteAttributeValue;
+  } else {
+    currentAttribute.value += c;
+  }
+}
+
+function singleQuotedAttributeValue(c) {}
+
+function afterQuteAttributeValue(c) {}
+
+function UnquotedAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return beforeAttributeName;
+  } else if (c === "/") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    return selfClosingStartTag;
+  } else if (c === ">") {
+    currentToken[currentAttribute.name] = currentAttribute.value;
+    emit(currentToken);
+    return data;
+  } else if (c === "\u0000") {
+  } else if (c === '"' || c === "'" || c === "<" || c === "=" || c === "`") {
+  } else if (c === EOF) {
+  } else {
+    currentAttribute.value += c;
+    return UnquotedAttributeValue;
+  }
+}
+
 function selfClosingStartTag(c) {
   if (c === ">") {
     currentToken.isSelfClosing = true;
-    return true;
+    emit(currentToken);
+    return data;
   } else if (c === EOF) {
   }
 }
