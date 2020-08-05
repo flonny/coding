9b587d9785d6d5b63af05113ac6c7c87443df2b9
fanyuedong <632296950@qq.com>
2020-08-05 18:02:07 +0800

    vue 3.0

---
+++ algorithm/src/stack/stack.spec.js
@@ -0,0 +1,15 @@
+import { stack } from '../stack'
+
+describe('stack/stack', () => {
+
+  it('should observe basic properties', () => {
+    let dummy
+    const counter = reactive({ num: 0 })
+    effect(() => (dummy = counter.num))
+
+    expect(dummy).toBe(0)
+    counter.num = 7
+    expect(dummy).toBe(7)
+  })
+
+})
